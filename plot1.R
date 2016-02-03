d4073a56ea897dbf44d386ef47b40cef885f7cec
vmyilsamy <veeraraghavanm@gamil.com>
2016-02-03 23:36:49 +0000

    added R files and png

---
+++ plot1.R
@@ -0,0 +1,14 @@
+library(data.table)
+url <- "./household_power_consumption.txt"
+powerCons <- read.table(url, skip = 66637, nrows = 2880, header = FALSE, colClasses = "character", sep = ";")
+
+colnames(powerCons) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+powerCons$Date <- as.Date(powerCons$Date, format="%d/%m/%Y")
+datetime <- paste(powerCons$Date, powerCons$Time)
+powerCons$Datetime <- as.POSIXct(datetime)
+powerCons$Global_active_power <- as.numeric(powerCons$Global_active_power)
+
+png(file = "plot1.png", width = 480, height = 480, units = "px")
+with(powerCons, hist(Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,23 @@
+library(data.table)
+url <- "./household_power_consumption.txt"
+powerCons <- read.table(url, skip = 66637, nrows = 2880, header = FALSE, colClasses = "character", sep = ";")
+
+colnames(powerCons) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+powerCons$Date <- as.Date(powerCons$Date, format="%d/%m/%Y")
+datetime <- paste(powerCons$Date, powerCons$Time)
+powerCons$Datetime <- as.POSIXct(datetime)
+powerCons$Global_active_power <- as.numeric(powerCons$Global_active_power)
+powerCons$Sub_metering_1 <- as.numeric(powerCons$Sub_metering_1)
+powerCons$Sub_metering_2 <- as.numeric(powerCons$Sub_metering_2)
+powerCons$Sub_metering_3 <- as.numeric(powerCons$Sub_metering_3)
+
+png(file = "plot3.png", width = 480, height = 480, units = "px")
+with(powerCons, 
+     {
+         plot(Sub_metering_1~Datetime, type="l", xlab="", ylab = "Energy sub metering")
+         lines(Sub_metering_2~Datetime, col = "red")
+         lines(Sub_metering_3~Datetime, col = "blue")
+         legend("topright", lty=c(1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+     })
+dev.off()
